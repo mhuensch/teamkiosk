b11e733cb2877e04c7c6ecbf64f05d9a721b1747
Run00 <setup@run00.com>
2015-06-26 22:46:04 -0500

    Re-adding the coverage bars

---
+++ app/components/animated-bar.js
@@ -0,0 +1,78 @@
+App.AnimatedBarComponent = Ember.Component.extend({
+
+	tagName: 'span'
+
+	,classNames: ['animated-bar'],
+
+	onValueChange: function() {
+		var from = this.get('displayValue');
+		var to = this.get('value');
+
+		this.set('displayValue', this.get('value'));
+
+		if (!this.$()) return;
+
+		var box = this.$();
+
+		if (this.isAnimationRunning) {
+			//from = parseInt(paragraph.html(), 10);
+			box.velocity('stop');
+		}
+
+		box.velocity(
+			{
+				tween: [to, from]
+			}
+			,{
+				progress: function(elements, complete, remaining, start, tweenValue) {
+					box.css('width', tweenValue + '%');
+
+					var value = tweenValue/100/2 + .5;
+
+					var rgba = box.css('background-color')
+						.replace('rgb(', '')
+						.replace('rgba(', '')
+						.replace(')', '')
+						.split(',');
+					rgba[3] = value;
+
+					box.css('background-color', 'rgba('+rgba.join(', ')+')');
+
+
+				}.bind(this),
+				complete: function() {
+					this.isAnimationRunning = false;
+				}.bind(this),
+				begin: function() {
+					this.isAnimationRunning = true;
+				}.bind(this),
+				duration: 800,
+				easing: 'ease-out',
+				delay: 50
+			}
+		);
+
+	}.observes('value').on('init'),
+
+
+	onDidInsertElement: function() {
+		var box = this.$();
+		box.css('width', this.get('displayValue') + '%');
+		var value = this.get('displayValue')/100/2 + .5;
+
+		var rgba = box.css('background-color')
+			.replace('rgb(', '')
+			.replace(')', '')
+			.split(',');
+		rgba[3] = value;
+
+		box.css('background-color', 'rgba('+rgba.join(', ')+')');
+
+	}.on('didInsertElement'),
+
+
+	onWillDestroyElement: function() {
+		this.$().velocity('stop');
+	}.on('willDestroyElement')
+
+});
