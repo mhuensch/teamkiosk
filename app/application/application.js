43a9ffe995fc3de1f2a61de8854817b6bbeec9ba
Run00 <setup@run00.com>
2015-07-02 00:52:54 -0500

    Finished refactor of application code

--- app/application/application.js
+++ app/application/application.js
@@ -7,7 +7,7 @@
 App.ApplicationRoute = Ember.Route.extend({
 	actions: {
 		willTransition: function(transition) {
-			$('#app-nav-trigger').prop("checked", false);
+			$('#application-nav-trigger').prop("checked", false);
 			return true;
 		}
 	}
@@ -15,11 +15,11 @@ App.ApplicationRoute = Ember.Route.extend({
 
 App.ApplicationView = Ember.View.extend({
 	// Add the 'main' class to our primary ember view.
-	classNames: ['app']
+	classNames: ['application']
 
 	,didInsertElement: function() {
 		$('.header > ul > li > a').click(function(){
-			 $('#app-nav-trigger').prop("checked", false);
+			 $('#application-nav-trigger').prop("checked", false);
 		});
 	}
 });
