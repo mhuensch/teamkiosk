93f87f09853aec731b822a44e85d2789c1c7cc60
Michael Huensch <setup@run00.com>
2015-08-03 21:40:42 -0500

    Removing property watcher

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,12 @@
 // BUILD FILE
 // ---------------------------------------------------------------------------------------------------------------------
 
+// Used to fix the warning behavior in gulp-watch.  Without this line a warning will be issued that states:
+// "(node) warning: possible EventEmitter memory leak detected. 11 change listeners added."
+// This is "normal" when you are processing more than 10 files in parallel.  No memory is actually being leaked.
+// SEE: https://github.com/cbarrick/gulp-run/issues/7
+// TODO: remove with latest version of pyre
+require('events').EventEmitter.defaultMaxListeners = 0;
+
 // SEE: https://github.com/billowlabs/pyre for more information on pyre
 var gulp = require('pyre')(require('gulp'));
-
-
-// TODO: @pyre add this?? or fix bug where watchers warning is exceeded?
-process.stdin.setMaxListeners(0)
